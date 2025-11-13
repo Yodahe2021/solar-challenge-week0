67de231ef28075dcdd9659ad84b2f41f1940edf4
YODAHE TSEGAYE <95578751+Yodahe2021@users.noreply.github.com>
2025-11-13 14:55:21 +0300

    streamlitaac

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,23 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.title("🎈 My new Streamlit app")
+
+import pandas as pd
+
+@st.cache_data
+def load_data(country):
+    return pd.read_csv(f"data/clean/{country}-clean.csv")
+
+country = st.selectbox("Select Country", ["sierraleone", "benin", "togo"])
+df = load_data(country)
+st.write(df.head())
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+variable = st.selectbox("Select variable to visualize", ["GHI", "DNI", "DHI", "Tamb"])
+
+fig, ax = plt.subplots()
+sns.lineplot(data=df, x="Timestamp", y=variable, ax=ax)
+st.pyplot(fig)
+st.subheader("Summary Statistics")
+st.write(df.describe())
